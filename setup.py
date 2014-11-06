32d64d89b05f235a9d86d828df7b207fe7725590
Olafur Arason <olafura@reontech.com>
2014-11-06 11:02:38 +0000

    Update the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sundaytasks',
-    version='0.01',
+    version='0.02',
 
     description='SundayTasks is pluggable architecture for CouchDB',
 
